899dc49469991ddb839790ef76f5575ccbe98066
Moch Yayan Juan Alvredo XD <mikatoxd99@gmail.com>
2021-05-27 07:32:38 +0000

    Create asw.py

---
+++ asw.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python2
+# coding=utf-8
+
+import os
+try:
+    os.system('clear')
+    print '\n [!] sabar Tod script sedang di perbaiki:v\n'
+    os.system('git pull')
+    os.system('python2 ymbf.py')
