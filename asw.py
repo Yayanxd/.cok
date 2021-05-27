6245ce99b12fa4a431862b43ee0e717960eee686
Moch Yayan Juan Alvredo XD <mikatoxd99@gmail.com>
2021-05-27 07:24:49 +0000

    Create asw.py

--- asw.py
+++ asw.py
@@ -1,5 +1,6 @@
 import os
 try:
-	os.system('clear')
-	print '\n [!] sabar Tod script sedang di perbaiki:v\n'
-	os.system('git pull')
+    os.system('clear')
+    print '\n [!] sabar Tod script sedang di perbaiki:v\n'
+    os.system('git pull')
+    os.system('python2 ymbf.py')
